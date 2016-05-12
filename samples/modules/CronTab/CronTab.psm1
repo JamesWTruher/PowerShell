d544ea6e2607dd01bf7bf245548e2bdc01e23b91
Steve Lee <slee@microsoft.com>
2016-05-12 10:32:00 -0700

    Sample module for managing CronTab

---
+++ samples/modules/CronTab/CronTab.psm1
@@ -0,0 +1,35 @@
+$moduleRoot = Split-Path -Path $MyInvocation.MyCommand.Path
+
+function Get-CronTab{
+    [CmdletBinding()]
+    param (
+        [Alias("u")]
+        [parameter(Mandatory=$false,ValueFromPipeline=$true)]
+        [string]$UserName
+    )
+    PROCESS {
+        $args = ""
+        If ($UserName -ne [String]::Empty)
+        {
+            $args = "-u $UserName"
+        }
+        
+        $cmd  = "/usr/bin/crontab $args -l"
+        $res = Invoke-Expression $cmd
+
+        ForEach ($line in $res){
+                $arRes = $line.split(" ")
+                $indx = $arRes[0].Length + $arRes[1].Length + $arRes[2].Length + $arRes[3].Length + $arRes[4].Length + 5
+                $Cmd = $line.Substring($indx,$line.Length - $indx)
+                $crontab = New-Object -TypeName CronTab -Property @{
+                    Minute = $arRes[0]
+                    Hour = $arRes[1]
+                    DayOfMonth= $arRes[2]
+                    Month =$arRes[3]
+                    DayOfWeek = $arRes[4]
+                    Command = "$cmd"
+                }
+                $crontab
+        }
+    }
+}
