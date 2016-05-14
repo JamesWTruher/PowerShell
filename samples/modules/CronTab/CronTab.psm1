a71158f99bc3e4be00005a931e9a13662bc1ae76
Steve Lee <slee@microsoft.com>
2016-05-13 22:29:07 -0700

    Added manifest and format file

--- samples/modules/CronTab/CronTab.psm1
+++ samples/modules/CronTab/CronTab.psm1
@@ -1,27 +1,29 @@
 $moduleRoot = Split-Path -Path $MyInvocation.MyCommand.Path
 
-enum DayOfWeek{
-    
-}
-
-function New-CronJob {
+Function New-CronJob {
     [CmdletBinding()]
-    param (
-        [int] $Minute,
-        [int] $Hour,
-        [int] $DayOfMonth,
-        [int] $Month,
-        [int] $DayOfWeek,
-        [string] $Command
+    PARAM (
+        [Alias("u")][Parameter(Mandatory=$false,ValueFromPipeline=$true)]
+        [string] $UserName,
+        [Alias("mi")][string] $Minute = "*",
+        [Alias("h")][string] $Hour = "*",
+        [Alias("dm")][string] $DayOfMonth = "*",
+        [Alias("mo")][string] $Month = "*",
+        [Alias("dw")][string] $DayOfWeek = "*",
+        [Alias("c")][Parameter(Mandatory=$true)][string] $Command
     )
+    PROCESS {
+        $line = "{0} {1} {2} {3} {4} {5}" -f $Minute, $Hour, $DayOfMonth, $Month, $DayOfWeek, $Command
+        
+    }
 }
 
-function Get-CronJob {
+Function Get-CronJob {
     [CmdletBinding()]
-    param (
+    PARAM (
         [Alias("u")]
         [parameter(Mandatory=$false,ValueFromPipeline=$true)]
-        [string]$UserName
+        [string] $UserName
     )
     PROCESS {
         $args = ""
@@ -33,18 +35,20 @@ function Get-CronJob {
         $res = Invoke-Expression $cmd
 
         ForEach ($line in $res) {
-                $arRes = $line.split(" ")
-                $indx = $arRes[0].Length + $arRes[1].Length + $arRes[2].Length + $arRes[3].Length + $arRes[4].Length + 5
-                $Cmd = $line.Substring($indx,$line.Length - $indx)
-                $cronjob = New-Object -TypeName CronTab -Property @{
+            if ($line.Trim().Length -gt 0)
+            {
+                $arRes = $line.split(" ", 6)
+                $cronjob = New-Object -TypeName PSObject -Property @{
                     Minute = $arRes[0]
                     Hour = $arRes[1]
                     DayOfMonth= $arRes[2]
                     Month =$arRes[3]
                     DayOfWeek = $arRes[4]
-                    Command = "$cmd"
+                    Command = $arRes[5]
                 }
+                $cronjob.psobject.TypeNames.Insert(0,"CronJob")
                 $cronjob
+            }
         }
     }
 }
