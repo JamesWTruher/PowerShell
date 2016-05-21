57af8c4d2f35b20a97de812a8496680bd0598552
Steve Lee <slee@microsoft.com>
2016-05-20 20:28:21 -0700

    fixed New-CronJob to handle empty crontab case

--- samples/modules/CronTab/CronTab.psm1
+++ samples/modules/CronTab/CronTab.psm1
@@ -2,13 +2,20 @@ $crontabcmd = "/usr/bin/crontab"
 
 # Internal helper functions
 
-function Invoke-CronTab ([String] $user, [String] $args) {
-    If ($UserName -ne [String]::Empty) {
-        $args = "-u $UserName $args"
+function Invoke-CronTab ([String] $user, [String] $arguments, [Switch] $noThrow) {
+    If ($user -ne [String]::Empty) {
+        $arguments = "-u $UserName $arguments"
     }
     
-    $cmd  = "$crontabcmd $args"
-    Invoke-Expression $cmd
+    $cmd  = "$crontabcmd $arguments"
+    Write-Verbose $cmd
+    $output = Invoke-Expression $cmd 2>&1
+    if ($LastExitCode -ne 0 -and -not $noThrow) {
+        $e = New-Object System.InvalidOperationException -ArgumentList $output
+        throw $e
+    } else {
+        $output
+    }
 }
 
 function Import-CronTab ([String] $user, [String] $cronTab) {
@@ -57,19 +64,30 @@ function New-CronJob {
         [Alias("c")][Parameter(Mandatory=$true)][String] $Command
     )
     process {
+        # TODO: validate parameters, one complexity is different versions support different capabilities
         $line = "{0} {1} {2} {3} {4} {5}" -f $Minute, $Hour, $DayOfMonth, $Month, $DayOfWeek, $Command
-        
+        $crontab = (Invoke-CronTab -user $UserName -arguments "-l" -noThrow) + [Environment]::NewLine
+        if ($crontab -is [System.Management.Automation.ErrorRecord]) {
+            if ($crontab.Exception.Message.StartsWith("no crontab for ")) {
+                $crontab = [String]::Empty
+            }
+            else {
+                throw $crontab.Exception
+            }
+        }
+        $crontab += $line
+        Import-CronTab -User $UserName -crontab $crontab
     }
 }
 
 function Get-CronJob {
     [CmdletBinding()]
-    [OutputType([Cron.Job])]
+    [OutputType([PSObject])]
     param (
         [Alias("u")][Parameter(Mandatory=$false,ValueFromPipeline=$true)][String] $UserName
     )
     process {
-        $crontab = Invoke-CronTab -user $UserName -args "-l"
+        $crontab = Invoke-CronTab -user $UserName -arguments "-l"
 
         ForEach ($line in $crontab) {
             if ($line.Trim().Length -gt 0)
